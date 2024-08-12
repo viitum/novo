2210cd23992d5daf5d177fc56fd16570219ce8bd
Victor Hugo <victor.dti@uniube.br>
2024-08-12 20:27:03 -0300

    vitum

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo 'teste';
-?>
+<head>
+
+</head>
+<body>
+    <form method="post" action="gravar.php">
+        <input type="text" name="name">
+        <input type="submit" value="enviar">
+</form>
+</body>
+</html>
